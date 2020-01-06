d7f7754dd5e79c5b43e60be93d98b4c135d9a6d9
Shreyas <dlshreyas30@gmail.com>
2020-01-07 03:15:51 +0530

    Added two Screens

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
-import HomeScreen from './src/screens/HomeScreen';
+import HomeScreen from './src/screens/HomeScreen'
+import AddBillsScreen from './src/screens/AddBillsScreen'
+import ViewBillsScreen from './src/screens/ViewBillsScreen'
 
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen
+    Home : HomeScreen,
+    AddBill : AddBillsScreen,
+    ViewBill : ViewBillsScreen
   },
   {
     initialRouteName: 'Home',
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,25 @@
 import React from 'react'
-import {Text} from 'react-native'
+import { Text,View,Button,StyleSheet } from 'react-native'
 
-const HomeScreen = () => {
-	return <Text>Heloorflendfk</Text>
+const HomeScreen = ({navigation}) => {
+	return (
+		<View>
+			<Text style = {styles.titleStyle}>Expense Manager</Text>
+			<Text>Tap tyhe following buttons to go to appropriate section </Text>
+			<Button
+				title = 'Add Bills'
+				onPress = {() => navigation.navigate('AddBill')}
+			/>
+			<Button
+				title = 'View Bills'
+				onPress = {() => navigation.navigate('ViewBill')}
+			/>
+		</View>
+		)
 }
-
+const styles = StyleSheet.create({
+	titleStyle:{
+		fontSize : 30
+	}
+})
 export default HomeScreen
